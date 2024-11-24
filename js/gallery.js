3261269b16eac65e5430b41c3f18c617ca28a43b
Gusarova Anna <gusssanna@bk.ru>
2024-11-24 03:07:05 +0300

    Добавляет открытие во весь экран фотографий

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,24 @@
+import{ showBigPhoto } from './big-picture.js';
+
 const containerPhotos = document.querySelector('.pictures');
 const similarPhotosTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderPhoto = ({url, description, likes, comments}) => {
+const renderPhoto = (picture) => {
+  const {url, description, likes, comments} = picture;
   const photoElement = similarPhotosTemplate.cloneNode(true);
 
   photoElement.querySelector('.picture__img').src = url;
-  photoElement.querySelector('.picture__img').alt = description;
+  // photoElement.querySelector('.picture__img').alt = description;
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
+
+  const onPhotoElementClick = (evt) => {
+    evt.preventDefault();
+
+    showBigPhoto(picture);
+  };
+
+  photoElement.addEventListener('click', onPhotoElementClick);
 
   return photoElement;
 };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import './util.js';
 import {addPhotos} from './data.js';
 import {renderPhotos} from './gallery.js';
+import './big-picture.js';
 
 renderPhotos(addPhotos);
+
