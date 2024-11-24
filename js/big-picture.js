9b1fc60b2a22e1f348e24e4933d72fbef950f142
Gusarova Anna <gusssanna@bk.ru>
2024-11-24 14:25:40 +0300

    Добавляет комментарии к фотографии

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,4 +1,4 @@
-import { closeOnEscDown } from './util';
+import { closeOnEscDown } from './util.js';
 import './gallery.js';
 
 const COMMENTS_STEP = 5;
@@ -19,6 +19,51 @@ let commentsCount = COMMENTS_STEP;
 let currentComments = [];
 
 
+const createComment = (comment) => {
+  const newComment = document.createElement('li');
+  const imgComment = document.createElement('img');
+  const textComment = document.createElement('p');
+
+  newComment.classList.add('social__comment');
+  imgComment.classList.add('social__picture');
+  textComment.classList.add('social__text');
+
+  imgComment.src = comment.avatar;
+  imgComment.alt = comment.name;
+  textComment.textContent = comment.message;
+
+  newComment.append(imgComment);
+  newComment.append(textComment);
+
+  commentFragment.append(newComment);
+
+};
+
+const renderComments = () => {
+  socialComments.innerHTML = '';
+  socialCommentCount.innerHTML = '';
+
+  commentsCount = (commentsCount > currentComments.length) ? currentComments.length : commentsCount;
+  const commentsSelected = currentComments.slice(0, commentsCount);
+
+  if (currentComments.length <= COMMENTS_STEP || commentsCount >= currentComments.length) {
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  socialCommentCount.innerHTML = `${commentsCount} из <span class="social__comment-total-count">${currentComments.length}</span>`;
+  commentsSelected.forEach(createComment);
+
+  socialComments.append(commentFragment);
+};
+
+const onSocialCommentsClick = () => {
+  commentsCount += COMMENTS_STEP;
+  renderComments();
+};
+
+
 const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
@@ -34,9 +79,10 @@ const onBigPictureEscDown = (evt) => {
     closeBigPicture();
 
     document.removeEventListener('keydown', onBigPictureEscDown);
-    // socialComments.removeEventListener('click', onSocialCommentsClick);
+    socialComments.removeEventListener('click', onSocialCommentsClick);
   });
 };
+
 
 const onPictureCloseButtonClick = () => {
   closeBigPicture();
@@ -46,7 +92,7 @@ const onPictureCloseButtonClick = () => {
 
 
 const showBigPhoto = (picture) => {
-  const {url, description, likes} = picture;
+  const {url, description, likes, comments} = picture;
 
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
@@ -55,10 +101,15 @@ const showBigPhoto = (picture) => {
   socialCaption.alt = description;
   likesCount.textContent = likes;
 
+  currentComments = comments.slice();
+
+  renderComments();
+
   document.addEventListener('keydown', onBigPictureEscDown);
 
 };
 
+commentsLoader.addEventListener('click', onSocialCommentsClick);
 pictureCloseButton.addEventListener('click', onPictureCloseButtonClick);
 
 
--- js/gallery.js
+++ js/gallery.js
@@ -4,11 +4,10 @@ const containerPhotos = document.querySelector('.pictures');
 const similarPhotosTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const renderPhoto = (picture) => {
-  const {url, description, likes, comments} = picture;
+  const {url, likes, comments} = picture;
   const photoElement = similarPhotosTemplate.cloneNode(true);
 
   photoElement.querySelector('.picture__img').src = url;
-  // photoElement.querySelector('.picture__img').alt = description;
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
 
