e41987af9c40788166f34bfb81c77f25fcf48ea8
Keks <keks@htmlacademy.ru>
2024-11-24 14:27:17 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/gallery.js
@@ -0,0 +1,35 @@
+import{ showBigPhoto } from './big-picture.js';
+
+const containerPhotos = document.querySelector('.pictures');
+const similarPhotosTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPhoto = (picture) => {
+  const {url, likes, comments} = picture;
+  const photoElement = similarPhotosTemplate.cloneNode(true);
+
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__comments').textContent = comments.length;
+
+  const onPhotoElementClick = (evt) => {
+    evt.preventDefault();
+
+    showBigPhoto(picture);
+  };
+
+  photoElement.addEventListener('click', onPhotoElementClick);
+
+  return photoElement;
+};
+
+const renderPhotos = (photos) => {
+  const photoFragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    photoFragment.append(renderPhoto(photo));
+  });
+
+  containerPhotos.append(photoFragment);
+};
+
+export {renderPhotos};
