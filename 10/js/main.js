d77ee555bfc1a20c1321079e4f1a3ffd3a14df39
Keks <keks@htmlacademy.ru>
2024-12-01 22:03:54 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,9 @@
+import './util.js';
+import {addPhotos} from './data.js';
+import {renderPhotos} from './gallery.js';
+import './big-picture.js';
+import './validation.js';
+import './effects.js';
+
+renderPhotos(addPhotos);
+
